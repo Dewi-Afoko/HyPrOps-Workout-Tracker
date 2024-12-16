860383c997c7f3b2a0b8257aa066ab89b0e8a872
Dewi Afoko <dewi.afoko@gmail.com>
2024-12-16 14:26:49 +0000

    created routes all DRY

--- backend/app.py
+++ backend/app.py
@@ -101,8 +101,6 @@ def create_app():
 
         user.add_workout(workout)
         user.save()
-        # print(f'app - {str(workout.user_id) =}')
-        # print(f'app - {user.id =}')
 
         return jsonify({'message' : f'{workout.workout_name} created by {user.username}'}), 201
         
@@ -252,18 +250,13 @@ def create_app():
         if 'exercise_name' not in data.keys():
             return jsonify({'error' : 'You need to specify an exercise'}), 400
         
-        username = get_jwt_identity()
-
-        user = User.objects(username=username).first()
-        if not user:
-            return jsonify({'error' : 'User not found'}), 400
+        user = find_user_from_jwt()
+        if tuple_checker(user):
+            return user
         
-        
-        workout = next((workout for workout in user.workout_list if str(workout.id) == workout_id), None)
-
-
-        if not workout:
-            return jsonify({'error' : 'Workout not found'}), 400
+        workout = find_single_workout(workout_id)
+        if tuple_checker(workout):
+            return workout
         
         set_order = (len(workout.set_dicts_list) + 1)
         
@@ -293,8 +286,6 @@ def create_app():
         if tuple_checker(workout):
             return workout
         
-        if not workout:
-            return jsonify({'error' : 'Workout not found'}), 400
 
         set_dict = next((set for set in workout.set_dicts_list if set.set_order == int(set_order)), None)
 
@@ -318,9 +309,6 @@ def create_app():
         workout = find_single_workout(workout_id)
         if tuple_checker(workout):
             return workout
-        
-        if not workout:
-            return jsonify({'error' : 'Workout not found'}), 400
 
         set_dict = next((set for set in workout.set_dicts_list if set.set_order == int(set_order)), None)
 
@@ -340,9 +328,6 @@ def create_app():
         workout = find_single_workout(workout_id)
         if tuple_checker(workout):
             return workout
-        
-        if not workout:
-            return jsonify({'error' : 'Workout not found'}), 400
 
         set_dict = next((set for set in workout.set_dicts_list if set.set_order == int(set_order)), None)
 
