8f9b8328adcc5a93cfbca05e36590b8502ee7173
Dewi Afoko <dewi.afoko@gmail.com>
2025-01-23 14:20:41 +0000

    explicitly state VITE API URL build

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,5 @@
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:5000"; // Default for local dev
+console.log("VITE_API_URL from import.meta.env:", import.meta.env.VITE_API_URL);
+console.log("Final API_BASE_URL:", API_BASE_URL);
 
 export default API_BASE_URL;
